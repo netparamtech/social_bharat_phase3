14bca79949e71878abe5cb20057dd423881d8944
Mahesh Shittlani <maheshshittlani@gmail.com>
2023-08-12 17:44:25 +0530

    axios config file updated with headers

--- src/admin/axios/apiConfig.js
+++ src/admin/axios/apiConfig.js
@@ -1,10 +1,34 @@
 import axios from 'axios';
+import { useSelector } from 'react-redux';
 
+// Create a base axios instance without default headers
 const apiConfig = axios.create({
   baseURL: '/api/admin', // Authentication-related API
-  headers: {
-    'Content-Type': 'application/json',
-  },
 });
 
-export default apiConfig;
+// Create a function to set headers, including optional authorization token
+const setHeaders = (token) => {
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+
+  if (token) {
+    headers['Authorization'] = `Bearer ${token}`;
+  }
+
+  return headers;
+};
+
+// Create an axios instance with the set headers function
+const apiWithHeaders = axios.create({
+  baseURL: '/api/admin', // Authentication-related API
+});
+
+// Interceptor to set headers before each request
+apiWithHeaders.interceptors.request.use((config) => {
+  const token = useSelector((state) => state.auth.token);
+  config.headers = setHeaders(token);
+  return config;
+});
+
+export { apiConfig, apiWithHeaders };

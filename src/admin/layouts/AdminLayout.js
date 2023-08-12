651e2a916219cce2c4aecf22f207720bdbfdd750
Mahesh Shittlani <maheshshittlani@gmail.com>
2023-08-12 17:52:51 +0530

    custom css added

--- src/admin/layouts/AdminLayout.js
+++ src/admin/layouts/AdminLayout.js
@@ -14,7 +14,6 @@ const AdminLayout = ({ children }) => {
           'https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.0/js/bootstrap.bundle.min.js',
           'https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js',
           '/admin/js/sb-admin-2.min.js'
-            
         ];
     
         // Create script elements for each source and append them to the body
@@ -39,8 +38,9 @@ const AdminLayout = ({ children }) => {
     <Helmet>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css" integrity="sha512-q3eWabyZPc1XTCmF+8/LuE1ozpg5xxn7iO89yfSOd5/oKvyqLngoNGsx8jq92Y8eXJ/IRxQbEC+FGSYxtk2oiw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
-        <link href="/admin/css/sb-admin-2.min.css" rel="stylesheet" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.2/css/bootstrap.min.css" integrity="sha512-rt/SrQ4UNIaGfDyEXZtNcyWvQeOq0QLygHluFQcSjaGB04IxWhal71tKuzP6K8eYXYB6vJV4pHkXcmFGGQ1/0w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link href="/admin/css/sb-admin-2.min.css" rel="stylesheet" />
+        <link href="/admin/css/custom.css" />
     </Helmet>
       <div id="wrapper">
         <Sidebar />

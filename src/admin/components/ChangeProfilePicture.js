4d7d9d854d443336ed8481d9ad63353b0ebdb563
unknown <netparam.rahul95@gmail.com>
2023-09-06 11:14:18 +0530

    updated

--- src/admin/components/ChangeProfilePicture.js
+++ src/admin/components/ChangeProfilePicture.js
@@ -1,18 +1,21 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { Card, Button } from 'antd';
+import { Card, Button, Input } from 'antd';
 import 'antd/dist/antd';
 
-import { updateProfilePicture } from '../services/AdminService';
+import { updateBasicProfile, updateProfilePicture, uploadImage } from '../services/AdminService';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { login,logout } from '../actions/authActions';
+import { login, logout } from '../actions/authActions';
 
 const ChangeProfilePicture = (props) => {
   const { user } = props;
+  const [name,setName] = useState('');
+  const [email,setEmail] = useState('');
   const [profilePreview, setProfilePreview] = useState(null);
+  const [profileTempUrl, setProfileTempUrl] = useState('');
   const [profilePhoto, setProfilePhoto] = useState(null);
 
-  const [errors,setErrors] = useState('');
+  const [errors, setErrors] = useState('');
   const token = useSelector((state) => state.auth.token);
 
   const fileInputRef = useRef(null);
@@ -21,25 +24,56 @@ const ChangeProfilePicture = (props) => {
   const dispatch = useDispatch();
 
   useEffect(() => {
+    setName(user.name||'');
+    setEmail(user.email||'');
     setProfilePreview(user.photo || '');
+    setProfileTempUrl(user.photo||'');
   }, [user])
 
   const handlePlusIconClick = () => {
     fileInputRef.current.click();
   };
 
-  const handleProfilePhoto = (e) => {
+  const handleProfilePhoto = async (e) => {
     const selectedFile = e.target.files[0];
     setProfilePreview(URL.createObjectURL(selectedFile));
+
+    const formData = new FormData();
+    formData.append('image', selectedFile);
+
+    try {
+      const response = await uploadImage(formData); // Make an API call to get temporary URL
+      if (response && response.status === 200) {
+        console.log(response.data.data)
+        setProfileTempUrl(response.data.data.image);
+      }
+    } catch (error) {
+      // Handle error or show an error message
+    }
+
     setProfilePhoto(selectedFile);
   };
 
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  }
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  }
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const formData = new FormData();
-    formData.append('image', profilePhoto);
+
+    const data = {
+      name,email,
+      photo:profileTempUrl,
+    }
+
+    console.log(data)
+
     try {
-      const response = await updateProfilePicture(formData);
+      const response = await updateBasicProfile(data);
       if (response && response.status === 200) {
         dispatch(login(response.data.data, token));
         window.location.href = '/admin/dashboard';
@@ -50,13 +84,13 @@ const ChangeProfilePicture = (props) => {
       }
       // Unauthorized
       else if (error.response && error.response.status === 401) {
-       dispatch(logout());
-       navigate('/admin')
+        dispatch(logout());
+        navigate('/admin')
       }
       else if (error.response && error.response.status === 500) {
-       // dispatch(logout());
-       // navigate('/admin')
-       }
+        // dispatch(logout());
+        // navigate('/admin')
+      }
     }
   }
 
@@ -79,9 +113,11 @@ const ChangeProfilePicture = (props) => {
           />
 
         </div>
-       
+
         <div className='mt-5'>
-        <Button type="primary" onClick={handleSubmit}>Save</Button>
+          <Input placeholder="Name" value={name} onChange={handleNameChange} />
+          <Input placeholder="Email" value={email} onChange={handleEmailChange} />
+          <Button type="primary" onClick={handleSubmit}>Save</Button>
         </div>
       </Card>
     </div>

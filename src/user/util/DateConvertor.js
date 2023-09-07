1a46c886b7955beab047727040dedbf696e2d7ee
unknown <netparam.rahul95@gmail.com>
2023-09-07 12:02:33 +0530

    updated

--- src/user/util/DateConvertor.js
+++ src/user/util/DateConvertor.js
@@ -6,16 +6,16 @@ export const yyyyMmDdFormat = (inputDate) => {
     const month = String(date.getMonth() + 1).padStart(2, '0'); // Month is zero-based
     const day = String(date.getDate()).padStart(2, '0');
 
-    return `${year}-${month}-${day}`;
+    return `${year}-${day}-${month}`;
 };
 
 //convert date string into dd/mm/yyyy formate
 export const ddmmyyyyFormat = (inputDate) => {
-    const date = new Date(inputDate);
-
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0'); // Month is zero-based
-    const day = String(date.getDate()).padStart(2, '0');
-
+    const parts = inputDate.split('-');
+ 
+    const year = parts[0];
+    const month = parts[1];
+    const day = parts[2];
     return `${day}/${month}/${year}`;
+  
 };

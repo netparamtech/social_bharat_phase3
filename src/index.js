30f303be4efbd345e293055fc13c41b1cfbb628b
unknown <netparam.rahul95@gmail.com>
2023-09-07 18:03:13 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from "react";
-import {createRoot} from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
 import userStore from "./user/store";
 import adminStore from "./admin/store";
+import { StyleSheetManager } from 'styled-components';
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
-    <Provider store = {adminStore}>
+  <Provider store={adminStore}>
+    <StyleSheetManager shouldForwardProp={(prop) => prop !== 'sortActive'}>
       <App />
-    </Provider>
+    </StyleSheetManager>
+  </Provider>
+
 );

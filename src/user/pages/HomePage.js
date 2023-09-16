a33fa5b6ea7c1c5027a250a244319985363c9804
Sushil <netparam.sushil@gmail.com>
2023-09-17 00:31:04 +0530

    update

--- src/user/pages/HomePage.js
+++ src/user/pages/HomePage.js
@@ -14,7 +14,7 @@ const HomePage = () => {
                 <Banner />
                 <OurPartner />
                 <WhySocial />
-                <PromoteBusiness />
+                
                 <Services />
                 
                 <Testimonials />

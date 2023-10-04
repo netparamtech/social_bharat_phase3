d1eb747165de2e58c7930f75d8446651b5c6013b
unknown <netparam.rahul95@gmail.com>
2023-10-04 17:34:08 +0530

    update

--- src/user/components/home/Testimonial.js
+++ src/user/components/home/Testimonial.js
@@ -8,7 +8,6 @@ const Testimonials = () => {
 
   const handleReviewClick = (e) => {
     e.preventDefault();
-    window.scrollTo(0, 0);
     navigate('/user/rating');
   }
 
@@ -69,7 +68,7 @@ const Testimonials = () => {
           <span className="carousel-control-next-icon" aria-hidden="true"></span>
           <span className="visually-hidden">Next</span>
         </button>
-        <a className="btn btn-primary" href="" onClick={handleReviewClick}>Share Your Review</a>
+        <a className="btn btn-primary hover-pointer" onClick={handleReviewClick}>Share Your Review</a>
 
       </div>
     </section>

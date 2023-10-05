0f6679cd457148ccf730bc903371b87ef45e4f63
unknown <netparam.rahul95@gmail.com>
2023-10-05 17:33:05 +0530

    updated

--- src/admin/components/communities/CommunitiesList.js
+++ src/admin/components/communities/CommunitiesList.js
@@ -1,20 +1,74 @@
 import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router';
-import { deleteCommunity, fetchAllCommunity, updateCommunityStatus } from '../../services/AdminService';
-import { useDispatch } from 'react-redux';
+import { Table } from 'antd';
+import {
+  deleteCommunity,
+  fetchAllCommunity,
+  updateCommunityStatus,
+} from "../../services/AdminService";
+import { useNavigate } from 'react-router-dom';
+import Search from 'antd/es/input/Search';
 
 const CommunitiesList = () => {
   const [data, setData] = useState([]);
-
+  const [page, setPage] = useState(1);
+  const [size, setSize] = useState(10);
+  const [totalRows, setTotalRows] = useState(0);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [sortField, setSortField] = useState('');
+  const [sortOrder, setSortOrder] = useState('');
+  const [defaultImage, setDefaultImage] = useState('img/no-photo.png');
   const navigate = useNavigate();
-  const dispatch = useDispatch();
 
-  const fetchCommunities = async () => {
+  const handlePageChange = (page) => {
+    setPage(page);
+  };
 
+  const handlePageSizeChange = (current, pageSize) => {
+    setSize(pageSize);
+  };
+
+  const handleSearchChange = (query) => {
+    setPage(1);
+    setSearchQuery(query);
+  };
+
+  const handleTableChange = (pagination, filters, sorter) => {
+    const newSortField = sorter.field || '';
+    let newSortOrder = sorter.order || '';
+
+    // If the same column is clicked again, toggle the sort order
+    if (sortField === newSortField) {
+      newSortOrder = sortOrder === 'asc' ? 'desc' : 'asc';
+    }
+
+    setSortField(newSortField);
+    setSortOrder(newSortOrder);
+  };
+
+
+  const fetchData = async () => {
     try {
       const response = await fetchAllCommunity();
+
+      setData(response.data.data);
+
+      setTotalRows(response.data.data.length);
+    } catch (error) {
+      if (error.response && error.response.status === 401) {
+        navigate('/admin');
+      }
+      else if (error.response && error.response.status === 500) {
+        let errorMessage = error.response.data.message;
+        navigate('/server/error', { state: { errorMessage } });
+      }
+    }
+  };
+
+  const handleCommunityToggleStatus = async (id) => {
+    try {
+      const response = await updateCommunityStatus(id);
       if (response && response.status === 200) {
-        setData(response.data.data);
+        fetchData();
       }
     } catch (error) {
       if (error.response && error.response.status === 401) {
@@ -22,144 +76,165 @@ const CommunitiesList = () => {
       }
       else if (error.response && error.response.status === 500) {
         let errorMessage = error.response.data.message;
-        navigate('/server/error', { state: { errorMessage} });
-      }
-    }
-
-  };
-
-  const handleStatusToggle = async (communityId) => {
-    try {
-      const response = await updateCommunityStatus(communityId);
-      if (response && response.status === 200) {
-        fetchCommunities();
-        
-      }
-    } catch (error) {
-      if (error.response && error.response.status === 401) {
-        navigate('/admin');
-      } else if (error.response && error.response.status === 500) {
-        let errorMessage = error.response.data.message;
-        navigate('/server/error', { state: { errorMessage} });
+        navigate('/server/error', { state: { errorMessage } });
       }
     }
   }
 
-  const handleDelete = async (loggedUserid) => {
+  const handleDelete = async (id) => {
     try {
-      const response = await deleteCommunity(loggedUserid); // Replace with your actual delete API endpoint
+      const response = await deleteCommunity(id);
       if (response && response.status === 200) {
-        // Fetch communities again to update the list
-        fetchCommunities();
-        
+        fetchData();
       }
     } catch (error) {
-      // Handle error cases
       if (error.response && error.response.status === 401) {
         navigate('/admin');
-      }else if (error.response && error.response.status === 500) {
+      }
+      else if (error.response && error.response.status === 500) {
         let errorMessage = error.response.data.message;
-        navigate('/server/error', { state: { errorMessage} });
+        navigate('/server/error', { state: { errorMessage } });
       }
     }
-  };
-
+  }
+ 
+  // Rest of the code for handleUserToggleStatus, handleDeleteEnquiry, formatDate, and columns remains the same
 
   useEffect(() => {
-    fetchCommunities();
+    fetchData();
   }, []);
 
-  return (
-    <div >
-      <div className="container-fluid">
+  const columns = [
+    {
+      title: 'S.No',
+      dataIndex: 'sno',
+      render: (text, record, index) => (page - 1) * size + index + 1,
+      width: 100,
+    },
 
-        <div className="d-sm-flex align-items-center justify-content-between mb-4">
-          <h1 className="h3 mb-0 text-gray-800">Communities</h1>
-          <a href="#" className="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"
-            onClick={(e) => {
-              e.preventDefault();
-              navigate('/admin/create/community')
-            }}
-          >
-            Create Community
+    {
+      title: 'Name', dataIndex: 'name',
+      sorter: true,
+      sortDirections: ['asc', 'desc'],
+      with:100,
+    },
+
+    {
+      title: 'Thumbnail Image', dataIndex: 'thumbnail_image',
+      render: (text, record) => (
+        <a href={record.thumbnail_image} target='_blank'>
+          <img
+            src={record.thumbnail_image ? record.thumbnail_image : defaultImage}
+            alt={record.name}
+            title={record.name}
+            className='small-img-user-list'
+          />
+        </a>
+      ),
+      width:200,
+    },
+
+    {
+      title: 'Banner Image', dataIndex: 'banner_image',
+      render: (text, record) => (
+        <a href={record.banner_image} target='_blank'>
+          <img
+            src={record.banner_image ? record.banner_image : defaultImage}
+            alt={record.name}
+            title={record.name}
+            className='small-img-user-list'
+          />
+        </a>
+      ),
+      width:200,
+    },
+
+    {
+      title: 'Status',
+      dataIndex: 'status',
+      render: (text, record) => (
+        <div>
+          {record.status === 'Active' ? (
+            <a
+              className="collapse-item m-2"
+              href="#"
+              onClick={(e) => {
+                e.preventDefault();
+                handleCommunityToggleStatus(record.id);
+              }}
+            >
+              <i className="fa fa-thumbs-up text-primary" title="Active" />
+            </a>
+          ) : (
+            <a
+              className="collapse-item text-secondary m-2"
+              href="#"
+              onClick={(e) => {
+                e.preventDefault();
+                handleCommunityToggleStatus(record.id);
+              }}
+            >
+              <i className="fa fa-thumbs-down" title="Inactive" />
+            </a>
+          )}
+        </div>
+      ),
+      fixed: 'right',
+      sorter: true,
+      sortDirections: ['asc', 'desc'],
+      width: 150,
+    },
+    {
+      title: 'Actions',
+      dataIndex: 'actions',
+      render: (text, record) => (
+        <div>
+          <a className="collapse-item" onClick={(e) => {
+            e.preventDefault(); // Prevent the default anchor tag behavior
+            navigate(`/admin/update/community/${record.id}`);
+          }}>
+            <i className="fa fa-edit mr-4" title='Edit' />
+          </a>
+
+          <a className="collapse-item" onClick={(e) => {
+            e.preventDefault(); // Prevent the default anchor tag behavior
+            handleDelete(record.id);
+          }}>
+            <i className="fa fa-trash" title='Delete' />
           </a>
         </div>
-        <div className="card table-responsive p-3">
+      ),
+      fixed: 'right',
+      width: 200,
+    },
+    // Rest of the columns definition
+  ];
 
+  return (
+    <div>
+      <Search
+        placeholder="Search"
+        allowClear
+        onSearch={handleSearchChange}
+        style={{ marginBottom: 20, width: 200 }}
+      />
+      <Table
+        title={() => 'Communities'}  // Set the title to 'Enquiries'
+        dataSource={data}
+        columns={columns}
+        pagination={{
+          current: page,
+          pageSize: size,
+          total: totalRows,
+          onChange: handlePageChange,
+          onShowSizeChange: handlePageSizeChange,
+        }}
+        onChange={handleTableChange}
 
-          <table id='community-list' className="table table-striped table-bordered">
-            <thead>
-              <tr>
-                <th scope="col">S.No</th>
-                <th scope="col">Title</th>
-                <th scope="col">Thumbnail Image</th>
-                <th scope="col">Banner Image</th>
-                <th scope="col">Status</th>
-                <th scope="col">Action</th>
-              </tr>
-            </thead>
-            <tbody>
-
-              {data && data.length > 0 ? (data.map((item, index) => (
-                <tr key={index}>
-                  <th scope="row">{index + 1}</th>
-                  <td>{item.name}</td>
-                  <td>
-                    {item.thumbnail_image ? (
-                      <a href={item.thumbnail_image} target='_blank'><img src={item.thumbnail_image} title="Thumbnail image" alt="Thumbnail" className="small-img-thumbnail" /></a>
-                    ) : (
-                      item.thumbnail_image
-                    )}
-                  </td>
-                  <td>
-                    {item.banner_image ? (
-                      <a href={item.banner_image} target='_blank'><img src={item.banner_image} title="Banner image" alt="Banner" className="small-img-thumbnail" /></a>
-                    ) : (
-                      item.banner_image
-                    )}
-                  </td>
-                  <td>
-                    {item.status === 'Active' ? (
-                      <a href = "#" onClick={() => handleStatusToggle(item.id)}>
-                        <i className="fa fa-thumbs-up text-primary" title="Active" />
-                      </a>
-                    ) : (
-                     <a href='#' onClick={() => handleStatusToggle(item.id)}>
-                       <i className="fa fa-thumbs-down text-secondary" title="Inactive" />
-                     </a>
-                    )}
-                  </td>
-                  <td key={item.id}>
-                    <div className="d-flex">
-                      <a className="collapse-item" href="#" onClick={(e) => {
-                        e.preventDefault(); // Prevent the default anchor tag behavior
-                        navigate(`/admin/update/community/${item.id}`);
-                      }}>
-                        <i className="fa fa-edit mr-4" title='Edit' />
-                      </a>
-
-                      <a className="collapse-item" href="#" onClick={(e) => {
-                        e.preventDefault(); // Prevent the default anchor tag behavior
-                        handleDelete(item.id);
-                      }}>
-                        <i className="fa fa-trash" title='Delete' />
-                      </a>
-                    </div>
-                  </td>
-                </tr>
-
-              ))) : (
-                <tr>
-                  <td colSpan="6">No data available</td>
-                </tr>
-              )}
-
-            </tbody>
-          </table>
-        </div>
-      </div>
+        rowKey={(record) => record.id}
+      // onChange={handleSearchChange}
+      />
     </div>
-
   );
 };
 

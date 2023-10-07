887b5a3a0018d093fdceed4caf24aa5a1398bc3b
Sushil <netparam.sushil@gmail.com>
2023-10-07 09:52:03 +0530

    update

--- src/user/pages/profile/update/JobInfoPage.js
+++ src/user/pages/profile/update/JobInfoPage.js
@@ -37,7 +37,7 @@ const JobInfoPage = () => {
 
     return (
         <UserLayout>
-            <div>
+            <div id="auth-wrapper">
                 <JobInfo user={user} />
             </div>
         </UserLayout>

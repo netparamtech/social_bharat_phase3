5e253989bfebdccbf230a2e8736dc0a2da00f61c
unknown <netparam.rahul95@gmail.com>
2023-10-13 10:44:43 +0530

    update

--- src/user/components/otp/LoginWithOtp.js
+++ src/user/components/otp/LoginWithOtp.js
@@ -68,12 +68,11 @@ const LoginWithOtp = (props) => {
     } catch (error) {
       if (error.response && error.response.status === 400) {
         setErrors(error.response.data.errors);
-        setErrorMessage(error.response.data.errorMessage);
       }
 
       //Unauthorized
       else if (error.response && error.response.status === 401) {
-        setErrorMessage("Invalid Otp");
+        setErrorMessage(error.response.data.message);
         setErrors("");
       }
 

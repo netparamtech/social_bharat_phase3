33830e34506481108a39feb915e21b4725d46c5d
Sushil <netparam.sushil@gmail.com>
2023-10-19 16:37:28 +0530

    update

--- src/user/components/home/ViewCommunity.js
+++ src/user/components/home/ViewCommunity.js
@@ -3,42 +3,38 @@ import { fetchCommunityWithNAME } from "../../services/userService";
 import { useEffect, useState } from "react";
 
 const ViewCommunity = () => {
-    const { name } = useParams();
-    const [data, setData] = useState({});
-    const fetchCommunity = async () => {
-        try {
-            const response = await fetchCommunityWithNAME(name)
-            if (response && response.status === 200) {
-                setData(response.data.data);
-            }
-        } catch (error) {
-
-        }
-    }
-    useEffect(() => {
-        fetchCommunity();
-    }, []);
-    return (
-        <div id="auth-wrapper" className="pt-5 pb-5">
-        <div className="container">
-          <div className="mx-auto">
-            <div className="">
-              <div className="row">
-                <div className="col-lg-8 col-md-10 col-sm-12 mx-auto">
-                  <div className="text-center">
-                    <img src={data.banner_image} className="img-fluid" alt="Banner" />
-                  </div>
-                  {data && (
-                    <div
-                      dangerouslySetInnerHTML={{ __html: data.community_archive }}
-                    />
-                  )}
-                </div>
-              </div>
+  const { name } = useParams();
+  const [data, setData] = useState({});
+  const fetchCommunity = async () => {
+    try {
+      const response = await fetchCommunityWithNAME(name);
+      if (response && response.status === 200) {
+        setData(response.data.data);
+      }
+    } catch (error) {}
+  };
+  useEffect(() => {
+    fetchCommunity();
+  }, []);
+  return (
+    <div id="auth-wrapper" className="pt-3">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12">
+            <div className="mb-5">
+              <img src={data.banner_image} className="img-fluid rounded-2" alt="Banner" />
+            </div>
+            <div className="pb-5">
+            {data && (
+              <div 
+                dangerouslySetInnerHTML={{ __html: data.community_archive }}
+              />
+            )}
             </div>
           </div>
         </div>
       </div>
-    );
-}
+    </div>
+  );
+};
 export default ViewCommunity;

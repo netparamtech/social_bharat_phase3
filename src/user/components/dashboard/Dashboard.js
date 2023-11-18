f0ed0be05d49752636121b428532e5cd2ba4cd55
unknown <netparam.rahul95@gmail.com>
2023-11-18 10:19:39 +0530

    update

--- src/user/components/dashboard/Dashboard.js
+++ src/user/components/dashboard/Dashboard.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
   const navigate = useNavigate();
+  useEffect(()=>{
+    window.scrollTo(0,0);
+  },[]);
   return (
     
     <div id="dashboard">

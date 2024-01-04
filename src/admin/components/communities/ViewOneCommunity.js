70652c90f852f55d0f262137e50ae3a8e88ec023
unknown <netparam.rahul95@gmail.com>
2024-01-04 09:55:43 +0530

    update

--- src/admin/components/communities/ViewOneCommunity.js
+++ src/admin/components/communities/ViewOneCommunity.js
@@ -1,40 +1,69 @@
 import { useEffect, useState } from "react";
-import { fetchOneCommunity } from "../../services/AdminService";
+import { createSubcast, fetchAllSubcastsInOneCommunity, fetchOneCommunity } from "../../services/AdminService";
 import { useNavigate, useParams } from "react-router-dom";
+import UpdateSubcastCommunityForm from "./UpdateSubcastCommunityForm";
+import { Divider } from "antd";
 
 const ViewOneCommunity = () => {
   const { id } = useParams();
   const [data, setData] = useState([]);
 
   const [subcast, setSubcast] = useState('');
+  const [isClicked, setIsClicked] = useState(false);
+  const [subcastId, setSubcastId] = useState('');
   const [errors, setErrors] = useState('');
 
   const navigate = useNavigate();
 
-  // const fetchOneActiveCommunity = async () => {
-  //   try {
-  //     const response = await fetchOneCommunity(id);
-  //     if (response && response.status === 200) {
-  //       setData(response.data.data);
-  //     }
-  //   } catch (error) {
-  //     if (error.response && error.response.status === 401) {
-  //       navigate("/admin");
-  //     } else if (error.response && error.response.status === 500) {
-  //       let errorMessage = error.response.data.message;
-  //       navigate("/server/error", { state: { errorMessage } });
-  //     }
-  //   }
-  // };
+  const handleSubcastClicked = (value, id) => {
+    setIsClicked(value);
+    console.log(id)
+    setSubcastId(id);
+  }
+
+  const fetchAllSubcasts = async () => {
+    try {
+      const response = await fetchAllSubcastsInOneCommunity(id);
+      if (response && response.status === 200) {
+        setData(response.data.data);
+      }
+    } catch (error) {
+      if (error.response && error.response.status === 401) {
+        navigate("/admin");
+      } else if (error.response && error.response.status === 500) {
+        let errorMessage = error.response.data.message;
+        navigate("/server/error", { state: { errorMessage } });
+      }
+    }
+  };
 
 
+  useEffect(() => {
+    fetchAllSubcasts();
+    setErrors('');
+  }, [isClicked]);
 
-  // useEffect(() => {
-  //   fetchOneActiveCommunity();
-  // }, []);
-
-  const handleSubmit = () => {
-
+  const handleSubmit = async () => {
+    const data = {
+      community_id: id,
+      subcast: subcast.toUpperCase(),
+    }
+    try {
+      const response = await createSubcast(data);
+      if (response && response.status === 201) {
+        fetchAllSubcasts();
+        setErrors('');
+      }
+    } catch (error) {
+      if (error.response && error.response.status === 400) {
+        setErrors(error.response.data.errors);
+      } else if (error.response && error.response.status === 401) {
+        navigate("/admin");
+      } else if (error.response && error.response.status === 500) {
+        let errorMessage = error.response.data.message;
+        navigate("/server/error", { state: { errorMessage } });
+      }
+    }
   }
 
   return (
@@ -45,6 +74,16 @@ const ViewOneCommunity = () => {
             <div className="card">
               <div className="card-header text-light bg-success fs-5 fw-bold">Registered Subcasts</div>
               <div className="card-body">
+                <div>
+                  {
+                    data.map((item, index) => (
+                     <>
+                      <li key={index} className="hover-pointer-admin hover-pointer-admin-green fs-5 fw-bold" onClick={() => handleSubcastClicked(true, item.subcast_id)}>{item.subcast}</li>
+                      <Divider/>
+                     </>
+                    ))
+                  }
+                </div>
 
               </div>
 
@@ -52,31 +91,40 @@ const ViewOneCommunity = () => {
 
           </div>
 
-          <div className="col-12 col-sm-6">
-            <div className="card">
-              <div className="card-header bg-success text-light fs-5 fw-bold">
-                Add Subcast
-              </div>
-              <div className="card-body">
-                <div className="form-group">
-                  <label className="fw-bold">Subcast</label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="name"
-                    value={subcast}
-                    onChange={(e) => setSubcast(e.target.value)}
-                    placeholder="Enter Subcast Name"
-                  />
-                  {errors.subcast && <span className="error">{errors.subcast}</span>}
+          <div className="col-12 col-sm-6 mt-2">
+            {
+              !isClicked ? (
+                <div className="card">
+                  <div className="card-header bg-success text-light fs-5 fw-bold">
+                    Add Subcast
+                  </div>
+                  <div className="card-body">
+                    <div className="form-group">
+                      <label className="fw-bold">Subcast</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="name"
+                        value={subcast}
+                        onChange={(e) => setSubcast(e.target.value)}
+                        placeholder="Enter Subcast Name"
+                      />
+                      {errors.subcast && <span className="error">{errors.subcast}</span>}
+                    </div>
+                    <button type="submit" className="btn btn-primary w-25 " onClick={handleSubmit}>
+                      Submit
+                    </button>
+                    <button type="submit" className="btn btn-primary m-2 " onClick={()=>navigate('/admin/communities')}>
+                      View All Communities
+                    </button>
+
+                  </div>
+
                 </div>
-                <button type="submit" className="btn btn-primary w-25 ">
-                  Submit
-                </button>
-
-              </div>
-
-            </div>
+              ) : (
+                <UpdateSubcastCommunityForm handleSubcastClicked={handleSubcastClicked} id={id} subcastId = {subcastId} />
+              )
+            }
 
           </div>
         </div>
